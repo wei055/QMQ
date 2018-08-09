6cc21e117618bfe993c4c551d187f5d5f83c65cd
wei055 <wei055@126.com>
2018-08-09 12:57:54 +0800

    Add files via upload

---
+++ web/js/confession.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+	$("#no").mouseenter(function() {
+		var text = new Array();
+		text[0] = "嘿嘿...";
+		text[1] = "来追我呀";
+		text[2] = "不要点我嘛";
+		text[3] = "不让你点";
+		text[4] = "求求你了";
+		text[5] = "我喜欢你呀";
+		text[6] = "555...";
+		text[7] = "辣么绝情";
+		text[8] = "同意我嘛";
+		text[9] = "好不好嘛";
+		text[10] = "加油加油";
+		text[11] = "我会努力的";
+		text[12] = "干嘛老追我";
+		text[13] = "生气啦^_^";
+		text[14] = "乖...点好呀";
+		var int = Math.floor(Math.random()*15);
+		var x = Math.random()*600;
+		var y = Math.random()*1200;
+		$("#div_no").offset({top:x, left:y});
+		$("#d_no").css("background-color", "yellow");
+		$("#d_no").text(text[int]);
+	});
+	$("#yes").click(function() {
+		location.href = "agree.html";
+	});
+});
+
+$(document).ready(function() {
+	setTimeout("typing()", 3000);
+});
+		
+var text = "嘿嘿......... 猪猪... 我... 喜欢你...... 做我女盆友吧！ 直接关窗口是小猪哦 ^_^...";
+var index = 0;
+var id;
+function typing() {
+	$("#typing").text("");
+	$("#typing").show();
+	typed();
+}
+
+result = "";
+function typed() {
+	result += text.charAt(index);
+	$("#typing").text(result + (index & 1 ? "_" : " "));
+	if(index < text.length-1) {
+		index++;
+		id = setTimeout("typed()", 250);
+	}else {
+		clearTimeout(id);
+	}
+}
+
+
---
+++ web/js/index.js
@@ -0,0 +1,43 @@
+// index.js
+$(document).ready(function() {
+	setTimeout("adjust()", 10500);
+});
+
+function adjust() {
+	var x = $("#flash_2").position();
+	x.top -= 200;
+	$("#flash_2").css("z-index", "-1");
+	$("#flash_2").offset(x);
+	var y = $("#prompt").position();
+	y.top -= 200;
+	$("#prompt").offset(y);
+	var z = $("form").position();
+	z.top -= 200;
+	$("form").offset(z);
+}
+
+var int = 0;
+function login() {
+	int++;
+	var date = document.getElementById("date").value;
+	var prompt = document.getElementById("prompt");
+	if(date == "") {
+		prompt.innerHTML = "请先选择生日再登录喔";
+		int = 0;
+	}else {
+		if(date != "1994-05-06") {
+			switch(int) {
+				case 1: prompt.innerHTML = "不对不对~农历生日啦^_^"; break;
+				case 2: prompt.innerHTML = "咋啦？自己生日都记不住吗？"; break;
+				case 3: prompt.innerHTML = "还是不对哦"; break;
+				case 4: prompt.innerHTML = "多点几次登录吧"; break;
+				case 8: prompt.innerHTML = "难道不是19940506吗^_^"; break;
+			}
+		}else {
+			setTimeout("location.href = 'web/confession.html'", 2000);
+			prompt.innerHTML = "你准备好了吗？嘿嘿嘿...";
+		}
+	}
+}
+
+
