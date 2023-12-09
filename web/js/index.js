a2ed5fcf802a58a7b364014da12040cccebf152c
wei055 <wei055@126.com>
2023-12-09 20:58:39 +0800

    Update index.js

--- web/js/index.js
+++ web/js/index.js
@@ -25,13 +25,13 @@ function login() {
 		prompt.innerHTML = "请先选择生日再登录喔";
 		int = 0;
 	}else {
-		if(date != "1993-05-06") {
+		if(date != "2000-05-23") {
 			switch(int) {
 				case 1: prompt.innerHTML = "不对不对~农历生日啦^_^"; break;
 				case 2: prompt.innerHTML = "咋啦？自己生日都记不住吗？"; break;
 				case 3: prompt.innerHTML = "还是不对哦"; break;
 				case 4: prompt.innerHTML = "多点几次登录吧"; break;
-				case 8: prompt.innerHTML = "难道不是19930506吗^_^"; break;
+				case 8: prompt.innerHTML = "难道不是20000523吗^_^"; break;
 			}
 		}else {
 			setTimeout("location.href = 'web/confession.html'", 2000);
